9a578ae25fdaeda779a68029da86f2bc4ce2c757
Ghost <xxjojo29xx@mailoo.org>
2016-09-23 10:13:24 +0200

    New commit

--- common.h
+++ common.h
@@ -12,19 +12,18 @@
 
 #define ASCII_DECAL_NUM 48
 
-void fatal(char *str);
-void critik(char *str);
+void fatal(char *str); // print error message and quit
+void critik(char *str); // print error message and continue
 
-int buflen(char *buf);
-int intlen(int nb);
-int rstbuf(char *buf);
+int buflen(char *buf); // (strlen)
+int intlen(int nb); // return len of nb. ex: 145 -> return 3
+int rstbuf(char *buf); // set all non-zero (at the first '\0') char to '\0'
 
-int print(char *str);
-int println(char *str);
-int print_i(int *array, int len);
+int print(char *str); // print on stdout *str
+int println(char *str); // same print() but add \n at the end
+int print_i(int *array, int len); // print int array in 10 columns
 
-char *itoa(char *dest, int nb);
-
+char *itoa(char *dest, int nb); // convert nb in string and copy them into *dest
 
 #include "common.c"
 #endif
